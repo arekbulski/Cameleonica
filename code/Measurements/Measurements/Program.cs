d43e05f6894fb59b55949ae23e622849fac53d74
Arkadiusz Bulski <arek.bulski@gmail.com>
2015-10-17 21:18:00 +0200

    Measurements: Program using INCREMENT mode

--- code/Measurements/Measurements/Program.cs
+++ code/Measurements/Measurements/Program.cs
@@ -45,12 +45,17 @@ namespace Measurements
 		{
 			long AREA = 0;
 			int AMOUNT = 0;
-			int BLOCK = 512;
+			int BLOCK = 32*1024;
+			int INCREMENT = 1 * 1024;
 
 			AREA = SuffixToBytes (args [0]);
 			AMOUNT = (int)Math.Min (AREA/512/20, 8000);
+			if (INCREMENT > 0) {
+				AMOUNT = 1024 * 1024 * 1024 / (INCREMENT + BLOCK);
+			}
+
 			Console.WriteLine ("Amount: " + AMOUNT + "  Block size: " + BLOCK + 
-			                   "  Area: " + AREA + " = " + BytesToSuffix(AREA));
+			                   "  Area: " + AREA + "  Increment: " + INCREMENT);
 
 			string diskpath = args [1];
 			int fd = Syscall.open (diskpath, OpenFlags.O_RDONLY);
@@ -66,6 +71,12 @@ namespace Measurements
 				locations [i] = (long)(gen.NextDouble () * AREA);
 			}
 
+			if (INCREMENT > 0) {
+				for (int i = 0; i < locations.Length; i++) {
+					locations [i] = (INCREMENT + BLOCK) * i;
+				}
+			}
+
 			foreach (var i in Enumerable.Range(0,AMOUNT)) {
 				Stopwatch watch = Stopwatch.StartNew ();
 				Syscall.pread (fd, buf, (ulong)BLOCK, locations [i]);
@@ -79,13 +90,17 @@ namespace Measurements
 //			});
 
 			var sum = times.Sum ();
-			var timesascending = times.OrderBy (x => x).ToArray ();
-			var min95 = timesascending [(int)(0.05 * AMOUNT)];
-			var max95 = timesascending [(int)(0.95 * AMOUNT)];
+//			var timesascending = times.OrderBy (x => x).ToArray ();
+//			var min95 = timesascending [(int)(0.05 * AMOUNT)];
+//			var max95 = timesascending [(int)(0.95 * AMOUNT)];
 
 			Console.WriteLine ("Total: " + sum + "   Average: " + sum / AMOUNT);
-			Console.WriteLine ("Min: " + times.Min () + "  of upper 95 percent: " + min95);
-			Console.WriteLine ("Max: " + times.Max () + "  of lower 95 percent: " + max95);
+//			Console.WriteLine ("Min: " + times.Min () + "  of upper 95 percent: " + min95);
+//			Console.WriteLine ("Max: " + times.Max () + "  of lower 95 percent: " + max95);
+
+			if (INCREMENT > 0) {
+				
+			}
 		}
 	}
 }
