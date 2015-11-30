d7410fffa08d95b4ab4ae2a26ffdc8178deeaf85
Arkadiusz Bulski <arek.bulski@gmail.com>
2015-11-30 09:53:06 +0100

    Measuring python script tweaked (and +x chmod)

--- code/Measurements Py/measure.py
+++ code/Measurements Py/measure.py
@@ -1,18 +1,19 @@
-import os, time, subprocess, random
+#!/usr/bin/python
+import os, time, random
 
 def BytesString(n):
-    suffixes = ['B','KB','MB','GB','TB']
+    suffixes = ['B','KB','MB','GB','TB','PB','EB','ZB','YB']
     suffix = 0
     while n % 1024 == 0:
         suffix += 1
         n /= 1024
-    return "{0}{1}".format(n, suffixes[suffix])
+    return '{0}{1}'.format(n, suffixes[suffix])
 
 KB,MB,GB,TB = 1024,1024**2,1024**3,1024**4
 
-disk = open("/dev/sdb", "r")
+disk = open('/dev/sdb', 'r')
 disksize = TB
-print "Entire disk has size: ", BytesString(disksize)
+print 'Entire disk has size: ', BytesString(disksize)
 
 for area in [1*MB,4*MB,16*MB,128*MB,1*GB,4*GB,16*GB,128*GB,1*TB]:
     os.system('echo noop | sudo tee /sys/block/sdb/queue/scheduler > /dev/null')
@@ -32,5 +33,5 @@ for area in [1*MB,4*MB,16*MB,128*MB,1*GB,4*GB,16*GB,128*GB,1*TB]:
         times.append(point)
 
     times = sorted(times)[:4750]
-    print "Area tested: {0:5}  Average: {1:6f}sec  Max: {2:6f}sec  Sum: {3:2f}sec".format(
+    print 'Area tested: {0:5}  Average: {1:6f}sec  Max: {2:6f}sec  Sum: {3:2f}sec'.format(
         BytesString(area), sum(times)/len(times), max(times), sum(times))
