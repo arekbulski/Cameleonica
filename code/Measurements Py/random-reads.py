c647837332c36716b3a95b408dc610705042f3cd
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-10 00:27:35 +0200

    random-reads.py displays more details, merged with random-reads-beginning; log updated

--- code/Measurements Py/random-reads.py
+++ code/Measurements Py/random-reads.py
@@ -18,32 +18,52 @@ def BytesInt(s):
             return int(s.rstrip(suffix))*1024**power
     raise ValueError('BytesInt requires proper suffix ('+' '.join(suffixes)+').')
 
+def BytesStringFloat(n):
+    x = float(n)
+    suffixes = ['B','KB','MB','GB','TB','PB','EB','ZB','YB']
+    suffix = 0
+    while x > 1024.0:
+        suffix += 1
+        x /= 1024.0
+    return '{0:0.2f}{1}'.format(x, suffixes[suffix])
+
+
+
 disk = open('/dev/sdb', 'r')
-disksize = BytesInt('1TB')
-samplesize = 250
+disk.seek(0,2)
+disksize = disk.tell()
+os.system('echo noop | sudo tee /sys/block/sdb/queue/scheduler > /dev/null')
+
+samplesize = 25
+randomareas = '-r' in sys.argv
 displaytimes = '-v' in sys.argv
 displaysamplesize = 24
 
-print 'Entire disk size: {0}  Sample size: {1}  (displayed {2})'.format(
-    BytesString(disksize), samplesize, 
-    format(displaysamplesize) if displaytimes else 'none')
+print 'Syntax: progam [-r] [-v]: for random areas, verbose mode.'
+print 'Disk name: {0}  Disk size: {1}\nRandom seek time {2}  Sample size: {3}  (displayed {4})'.format(
+    disk.name, BytesStringFloat(disksize),
+    'using random areas of disk.' if randomareas else 'using beginning of disk.', 
+    samplesize, format(displaysamplesize) if displaytimes else 'none')
 
-for area in map(BytesInt, '1MB 2MB 4MB 8MB 16MB 32MB 64MB 128MB 256MB 512MB 1GB 2GB 4GB 8GB 16GB 32GB 64GB 128GB 256GB 512GB 1TB'.split()):
-
-    os.system('echo noop | sudo tee /sys/block/sdb/queue/scheduler > /dev/null')
+for area in [BytesInt('1MB')*2**i for i in range(0,64)]+[disksize]:
+    if disksize < area:
+        break
+    
     os.system('echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null')
 
     times = []
+    disk.seek(0)
+    disk.read(512)
     for _ in range(samplesize):
-        left = random.randint(0, disksize-area)
+        left = random.randint(0, disksize-area) if randomareas else 0
         right = left + random.randint(0, area)
         disk.seek(left)
         disk.read(512)
         start = time.time()
         disk.seek(right)
         disk.read(512)
-        end = time.time()
-        times.append(end-start)
+        finish = time.time()
+        times.append(finish-start)
 
     times = sorted(times)[:samplesize*95/100]
     print 'Area tested: {0:5}   Average: {1:3.2f} ms   Max: {2:3.2f} ms   Sum: {3:0.2f} sec'.format(
