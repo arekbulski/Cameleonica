6a3798704f512f755ce9b19654be0cabf849ac10
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-10 12:34:56 +0200

    better displaying; log updated

--- code/Measurements Py/test-disk-reading.py
+++ code/Measurements Py/test-disk-reading.py
@@ -47,15 +47,16 @@ displaytimes = '-v' in sys.argv
 
 #--------------------------------------------------------------------------------------------------
 
-samplesize = 100
-displaysamplesize = 24
+bufsize = 512
+bufcount = 100
+displaysamplecount = 24
 
 for randomareas in [False,True]:
     print
     print 'Measuring: Random seek time {0}'.format(
         'using random areas of disk.' if randomareas else 'using beginning of disk.')
-    print 'Sample size: {0} {1}'.format(
-        samplesize, '(displayed {0})'.format(displaysamplesize) if displaytimes else '')
+    print 'Samples: {0}{1}   Sample size: {2}'.format(
+        bufcount, ' (displayed {0})'.format(displaysamplecount) if displaytimes else '', bufsize)
 
     for area in [BytesInt('1MB')*2**i for i in range(0,64)]+[disksize]:
         if area > disksize:
@@ -65,26 +66,26 @@ for randomareas in [False,True]:
 
         times = []
         disk.seek(0)
-        disk.read(512)
-        for _ in range(samplesize):
+        disk.read(bufsize)
+        for _ in range(bufcount):
             left = random.randint(0, disksize-area) if randomareas else 0
             right = left + random.randint(0, area)
             disk.seek(left)
-            disk.read(512)
+            disk.read(bufsize)
             start = time.time()
             disk.seek(right)
-            disk.read(512)
+            disk.read(bufsize)
             finish = time.time()
             times.append(finish-start)
 
-        times = sorted(times)[:samplesize*95/100]
+        times = sorted(times)[:bufcount*95/100]
         print 'Area tested: {0:6}   Average: {1:5.2f} ms   Max: {2:5.2f} ms   Total: {3:0.2f} sec'.format(
             BytesString(area) if area < disksize else BytesStringFloat(area), 
             sum(times)/len(times)*1000, max(times)*1000, sum(times))
         if displaytimes:
             print 'Read times: {0} ... {1} ms'.format(
-                ' '.join(['{0:0.2f}'.format(x*1000) for x in times[:displaysamplesize/2]]), 
-                ' '.join(['{0:0.2f}'.format(x*1000) for x in times[-displaysamplesize/2:]]))
+                ' '.join(['{0:0.2f}'.format(x*1000) for x in times[:displaysamplecount/2]]), 
+                ' '.join(['{0:0.2f}'.format(x*1000) for x in times[-displaysamplecount/2:]]))
 
 
 #--------------------------------------------------------------------------------------------------
