6240a1c5c3306d1a6fd8223b59dc07e6fb12f96f
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-10 12:35:10 +0200

    removed old C code as used in document

--- scripts/demofsync-pseudo.c
+++
@@ -1,8 +0,0 @@
-int fd = creat("file.new", S_IRUSR|S_IWUSR);
-write(fd, buf, 255);
-fsync(fd);
-close(fd);
-int dirfd = open(".", O_DIRECTORY);
-fsync(dirfd);
-close(dirfd);
-rename("file.new", "file");
