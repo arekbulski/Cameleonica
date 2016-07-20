32d6d578ec92deb9383d4445e94ac61d3693c8d0
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-20 15:59:48 +0200

    Mission statement updated; new section

--- documentation/extract.sh
+++ documentation/extract.sh
@@ -1,4 +1,4 @@
 pdftk combined.pdf cat 1-7 output extracted-mission.pdf
-pdftk combined.pdf cat 9-20 output extracted-ideas.pdf
-pdftk combined.pdf cat 21-23 output extracted-responses.pdf
+pdftk combined.pdf cat 9-21 output extracted-ideas.pdf
+pdftk combined.pdf cat 23-25 output extracted-responses.pdf
 
