8365a798b19b1773d4490c681e82bbd8f0d3e579
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-22 21:56:40 +0200

    Extract script to include empty tail pages.

--- documentation/extract.sh
+++ documentation/extract.sh
@@ -1,3 +1,3 @@
-pdftk combined.pdf cat 1-7 output extracted-mission.pdf
+pdftk combined.pdf cat 1-8 output extracted-mission.pdf
 pdftk combined.pdf cat 9-22 output extracted-ideas.pdf
-pdftk combined.pdf cat 23-25 output extracted-responses.pdf
+pdftk combined.pdf cat 23-26 output extracted-responses.pdf
