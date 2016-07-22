6c67267c902ac6227380be276f73e038df41ced2
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-22 14:14:46 +0200

    Mission statement: seek times table; pdfs updated

--- documentation/extract.sh
+++ documentation/extract.sh
@@ -1,4 +1,3 @@
 pdftk combined.pdf cat 1-7 output extracted-mission.pdf
-pdftk combined.pdf cat 9-21 output extracted-ideas.pdf
+pdftk combined.pdf cat 9-22 output extracted-ideas.pdf
 pdftk combined.pdf cat 23-25 output extracted-responses.pdf
-
