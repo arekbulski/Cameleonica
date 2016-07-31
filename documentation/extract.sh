e8401b7a395ea6a3b2ee6da8dfeae94ddf7cea71
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-08-01 01:16:49 +0200

    Updated PDFs.

--- documentation/extract.sh
+++ documentation/extract.sh
@@ -1,3 +1,3 @@
 pdftk combined.pdf cat 1-8    output extracted-mission.pdf
-pdftk combined.pdf cat 9-24   output extracted-ideas.pdf
-pdftk combined.pdf cat 25-28  output extracted-responses.pdf
+pdftk combined.pdf cat 9-26   output extracted-ideas.pdf
+pdftk combined.pdf cat 27-30  output extracted-responses.pdf
