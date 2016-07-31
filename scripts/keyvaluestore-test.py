6d9e5202fdc16dbc03348c4e6efed666a632f946
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-07-31 04:01:05 +0200

    KeyValue store: testing still unusable

--- scripts/keyvaluestore-test.py
+++ scripts/keyvaluestore-test.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 import keyvaluestore
 
-c = keyvaluestore.Container("/tmp/test-keyvaluestore")
-assert c.getkeys() == []
-c.set(1, 2)
-assert c.get(1) == ("any picklable object", 2)
-assert c.get("nonexisting") == None
-assert c.getkeys() == [1]
-c.revert()
-assert c.getkeys() == []
+
+def throws(code, errortype):
+    try:
+        code()
+    except errortype, e:
+        return True
+    else:
+        return False
+
+assert throws(lambda: 1, AssertionError)
+assert throws(lambda: open('???'), OSError)
+
+
+filename = '/tmp/test-keyvaluestore'
+
+with keyvaluestore.Container(filename) as c:
+    assert c.getkeys() == []
+    c.set(1, 2)
+    assert c.get(1) == ('any picklable object', 2)
+    assert c.get('nonexisting') == None
+    assert c.getkeys() == [1]
+    c.revert()
+    assert c.getkeys() == []
+
